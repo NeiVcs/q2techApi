796f8b168f9cc8ea3707da0cc07a12c3e50fe2fa
Valdinei <neivcs@hotmail.com>
2026-02-23 09:20:19 -0300

    adjust find all product

--- src/modules/product/services/FindAllProductService.ts
+++ src/modules/product/services/FindAllProductService.ts
@@ -3,27 +3,40 @@ import { FindAllProductInputDTO } from "@modules/product/dto/FindAllProductInput
 import { FindAllProductOutputDTO } from "@modules/product/dto/FindAllProductOutputDTO";
 import { ProductRepository } from '../data/ProductRepository';
 import { IProduct } from '../data/IProduct';
+import { AdditionalRepository } from '@modules/additional/data/AdditionalRepository';
+import { FindAllProductTransformer } from '@modules/product/transformers/FindAllProductTransformer';
 
 @singleton()
 export class FindAllProductService {
-  constructor(private storage: ProductRepository) { }
+  constructor(
+    private productStorage: ProductRepository,
+    private additionalStorage: AdditionalRepository,
+    private transformer: FindAllProductTransformer
+  ) { }
 
   public async execute(inputDTO: FindAllProductInputDTO): Promise<FindAllProductOutputDTO> {
-    const response = await this.storage.findAll();
-    this.findCategories(response)
+    const entity = await this.productStorage.findAll()
+    const outputDto = this.findCategories(entity)
 
-    return { items: response } as unknown as FindAllProductOutputDTO;
+    return { items: entity } as unknown as FindAllProductOutputDTO;
   }
 
-  private async findCategories(productList: IProduct[]) {
-    const promises = productList.map(async (el) => {
-      if (el.additionalListId) {
-        return await this.storage.findById(el.additionalListId);
-      }
-      return null;
-    });
+  private async findCategories(entity: IProduct[]) {
+    // const outputDto: FindAllProductOutputDTO = this.transformer.toApi({items: entity})
+    //
+    // const promises = entity.map(async (el) => {
+    //   if (el.additionalListId) {
+    //     const results = await this.additionalStorage.findById(el.additionalListId);
+    //
+    //     const product = outputDto.items.find(f => f.id === el.id);
+    //     product.additionalList = results;
+    //     console.log(product);
+    //   }
+    //   return null;
+    // });
+    //
+    // const results = await Promise.all(promises);
 
-    //const results = await Promise.all(promises);
     //const additionalListGroup = results.filter(item => item !== null);
     //return { items: additionalListGroup } as unknown as FindAllProductOutputDTO;
   }
