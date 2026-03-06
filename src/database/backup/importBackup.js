c41ef3f9aca8f87a3aaa87d6af8d575cb8ddae85
Valdinei <neivcs@hotmail.com>
2026-03-06 08:43:07 -0300

    add db backup

---
+++ src/database/backup/importBackup.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const fs = require('fs');
+
+async function importBackup() {
+  await mongoose.connect(process.env.MONGODB_URL);
+  const db = mongoose.connection.db;
+
+  const rawData = fs.readFileSync('dump_teste.json');
+  const backupData = JSON.parse(rawData);
+
+  for (const colName in backupData) {
+    if (backupData[colName].length > 0) {
+      // Limpa a coleção antes de importar para não duplicar IDs
+      await db.collection(colName).deleteMany({});
+
+      // Insere os dados
+      await db.collection(colName).insertMany(backupData[colName]);
+      console.log(`🚀 Coleção [${colName}]: ${backupData[colName].length} documentos importados.`);
+    }
+  }
+
+  console.log('✨ Restauração concluída com sucesso!');
+  process.exit();
+}
+
+importBackup();
+
+// node --env-file=.env src/database/backup/importBackup.js
