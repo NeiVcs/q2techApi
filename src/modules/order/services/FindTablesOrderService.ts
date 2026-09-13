b10f631bdad2bdb2fc958d56ff6d16050d31a41f
neivcs <neivcs@hotmail.com>
2026-09-13 20:40:02 -0300

    feat: adjust find tables order service

--- src/modules/order/services/FindTablesOrderService.ts
+++ src/modules/order/services/FindTablesOrderService.ts
@@ -17,18 +17,12 @@ export class FindTablesOrderService {
 
   public async execute(inputDTO: FindTablesOrderInputDTO): Promise<FindTablesOrderOutputDTO> {
     const company = await this.companyStorage.findById(inputDTO.companyId);
-    //TODO: incluir tables na empresa
-    const tableList = [
-      { id: '4b97150f-76f9-416d-af14-48174a630367', table: '1' },
-      { id: 'f89b4d98-e2ad-4a65-aa0d-6682bf4cffc8', table: '2' },
-      { id: '4e6288b2-6a60-42bc-87a0-2bbabd3ef954', table: '3' }
-    ]
 
-    if (tableList.length === 0) {
+    if (!company.tables || company.tables.length === 0) {
       throw new ResourceNotFoundException(`Não existem mesas cadastradas`);
     }
 
-    const response = await this.getTableData(inputDTO.companyId, tableList)
+    const response = await this.getTableData(inputDTO.companyId, company.tables)
 
     return response as unknown as FindTablesOrderOutputDTO;
   }
